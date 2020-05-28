630abea847b8c320a13bdbe79ef4efb6e41fd825
torrakkeres <62864061+torrakkeres@users.noreply.github.com>
2020-05-28 19:15:49 -0400

    Update SBBanker.cs

--- Scripts/VendorInfo/SBBanker.cs
+++ Scripts/VendorInfo/SBBanker.cs
@@ -36,7 +36,7 @@ namespace Server.Mobiles
                 if (Multis.BaseHouse.NewVendorSystem)
                 {
                     Add(new GenericBuyInfo("1062332", typeof(VendorRentalContract), 1252, 20, 0x14F0, 0x672));
-                    Add(new GenericBuyInfo("1159156", typeof(CommissionContractOfEmployment), 28127, 20, 0x14F0, 0x672));
+                    Add(new GenericBuyInfo("1159156", typeof(CommissionContractOfEmployment), 1252, 20, 0x14F0, 0x672));
                 }
 
                 Add(new GenericBuyInfo("1047016", typeof(CommodityDeed), 5, 20, 0x14F0, 0x47));
